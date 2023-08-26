dc7d306eeeff7f2478308985c6f3e1db3ec32c51
tom05919 <twang05919@gmail.com>
2023-08-26 12:42:09 -0500

    Added AI detection API and writing to text API
    deleted pervious file upload, added new file upload along with pen to text API, then added AI detection API

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import axios from "axios";
 import './App.scss'
-import FileUpload from './FileUpload/FileUpload'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [previewImage, setPreviewImage] = useState("");
+  const [imageFile, setImageFile] = useState({});
+  const [text, setText] = useState("");
+  const [real, setReal] = useState();
+  const [fake, setFake] = useState();
 
-  const [files, setFiles] = useState([{
-    name: 'myFile.pdf'
-  }])
+  function handleFileInputChange(event) {
+    setImageFile(event.target.files[0]);
+    setPreviewImage(URL.createObjectURL(event.target.files[0]));
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const form = new FormData();
+    form.append("Session", "string");
+    form.append("srcImg", imageFile);
+    const options = {
+      method: "POST",
+      url: "https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/",
+      headers: {
+        "content-type": "multipart/form-data",
+        "x-rapidapi-host": "pen-to-print-handwriting-ocr.p.rapidapi.com",
+        "x-rapidapi-key": import.meta.env.VITE_API_KEY_PEN,
+      },
+      data: form,
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        setText(response.data.value);
+        console.log(response);
+      })
+     .catch(function (error) {
+       console.error(error);
+     });
+     console.log(text);
+  }
+
+  function checkForAI () {
+    const options = {
+      method: 'POST',
+      url: 'https://ai-content-detector-ai-gpt.p.rapidapi.com/api/detectText/',
+      headers: {
+        'content-type': 'application/json',
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': '9133386cf9msh24b0ee9eea003c6p12addejsn8086f047f132',
+        'X-RapidAPI-Host': 'ai-content-detector-ai-gpt.p.rapidapi.com'
+      },
+      data: {
+        text: text,
+      }
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setReal(response.data.isHuman);
+        setFake(response.data.fakePercentage)
+        console.log(response);
+      })
+     .catch(function (error) {
+       console.error(error);
+     });
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p classname="title">Upload file</p>
-        <FileUpload files={files} setFiles={setFiles} />
+        <div>
+          <img src={previewImage} />
+          <form onSubmit={handleFormSubmit}>
+            <input type="file" onChange={handleFileInputChange}/>
+            <input type="submit" value="Extract Text" />
+          </form>
+          <p className="text">{text}</p>
+        </div>
+        <button onClick={checkForAI}>
+          check for AI
+        </button>
+        <p>real: {real}</p>
+        <p>fake: {fake}</p>
       </div>
     </>
   )
