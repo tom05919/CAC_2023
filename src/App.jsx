b9545f153a5b28aea09e960811f4a974e1a7def6
tom05919 <twang05919@gmail.com>
2023-10-11 11:40:00 -0500

    added two API, added text box
    added two API and averaged their outcome, added a textbox where user can change if there were any mistakes during transcription

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ function App() {
   const [previewImage, setPreviewImage] = useState("");
   const [imageFile, setImageFile] = useState({});
   const [text, setText] = useState("");
-  const [real, setReal] = useState();
-  const [fake, setFake] = useState();
+  const [realOne, setRealOne] = useState();
+  const [fakeOne, setFakeOne] = useState();
+  const [realTwo, setRealTwo] = useState();
+  const [fakeTwo, setFakeTwo] = useState();
+  var fakeAvg;
 
   function handleFileInputChange(event) {
     setImageFile(event.target.files[0]);
     setPreviewImage(URL.createObjectURL(event.target.files[0]));
+  }
+
+  function onTextChange (event) {
+    setText(event.target.value);
+    console.log(event.target.value);
   }
 
   function handleFormSubmit(event) {
@@ -29,11 +37,11 @@ function App() {
       },
       data: form,
     };
+
     axios
       .request(options)
       .then(function (response) {
         setText(response.data.value);
-        console.log(response);
       })
      .catch(function (error) {
        console.error(error);
@@ -41,14 +49,40 @@ function App() {
      console.log(text);
   }
 
-  function checkForAI () {
+  function zeroGPT () {
+    const options = {
+      method: 'POST',
+      url: 'https://zerogpt.p.rapidapi.com/api/v1/detectText',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': import.meta.env.VITE_API_KEY_AI,
+        'X-RapidAPI-Host': 'zerogpt.p.rapidapi.com'
+      },
+      data: {
+        input_text: text,
+      }
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setRealOne(response.data.data.is_human_written);
+        setFakeOne(response.data.data.is_gpt_generated);
+        console.log(response);
+      })
+     .catch(function (error) {
+       console.error(error);
+     });
+  }
+
+  function AIConDet() {
     const options = {
       method: 'POST',
       url: 'https://ai-content-detector-ai-gpt.p.rapidapi.com/api/detectText/',
       headers: {
         'content-type': 'application/json',
         'Content-Type': 'application/json',
-        'X-RapidAPI-Key': import.meta.env.VITE_API_KEY_AI,
+        'X-RapidAPI-Key': '9133386cf9msh24b0ee9eea003c6p12addejsn8086f047f132',
         'X-RapidAPI-Host': 'ai-content-detector-ai-gpt.p.rapidapi.com'
       },
       data: {
@@ -59,20 +93,25 @@ function App() {
     axios
       .request(options)
       .then(function (response) {
-        setReal(response.data.isHuman);
-        setFake(response.data.fakePercentage)
-        console.log(response);
+        console.log(response); //fakePercentage
+        setRealTwo(response.data.isHuman);
+        setFakeTwo(response.data.fakePercentage);
       })
      .catch(function (error) {
        console.error(error);
      });
   }
 
+  function avg() {
+    fakeAvg = (fakeOne + fakeTwo)/2;
+    return fakeAvg;
+  }
+
 
   return (
     <>
       <div className="card">
-        <p classname="title">Upload file</p>
+        <p className="title">Upload file</p>
         <div>
           <img src={previewImage} />
           <form onSubmit={handleFormSubmit}>
@@ -80,12 +119,20 @@ function App() {
             <input type="submit" value="Extract Text" />
           </form>
           <p className="text">{text}</p>
+          <textarea className="textarea"
+            defaultValue={text}
+            onInput={onTextChange}
+          />
         </div>
-        <button onClick={checkForAI}>
+        <button onClick={() => {
+          //zeroGPT ();
+          AIConDet();
+          //avg();
+        }}>
           check for AI
         </button>
-        <p>real: {real}</p>
-        <p>fake: {fake}</p>
+        <p>real: {100 - fakeTwo}</p>
+        <p>fake: {fakeTwo}</p>
       </div>
     </>
   )
